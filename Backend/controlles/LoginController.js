48c021ff8a88ab786eef38fbe0ede05a9cd86bfe
sohila-mohamed <sohailamohamed.sbme@gmail.com>
2021-05-05 21:30:22 +0200

    Login Controller v.01

---
+++ Backend/controlles/LoginController.js
@@ -0,0 +1,44 @@
+function AddNewUser(req, res) {
+    console.log("posting data  ", req.body);
+    console.log("posting file  ", req.file);
+
+    const _std = new req.DB_Scheme({
+        username: req.body.name,
+        age: req.body.age,
+        email: req.body.email,
+        city: req.body.city,
+        profileImg: req.file.filename
+    });
+    _std.save().then((result) => {
+        res.send(result);
+    }).catch((err) => {
+        console.log(err);
+    });
+}
+
+function UpdateUserData(req, res) {
+    console.log(req.file);
+    req.DB_Scheme.updateOne({ _id: req.params.id }, {
+            $set: {
+                username: req.body.name,
+                city: req.body.city,
+                age: req.body.age,
+                email: req.body.email,
+                profileImg: req.file.filename
+            }
+        })
+        .then((res1) => {
+            console.log(res1);
+            res.send(res1)
+        })
+        .catch((err1) => {
+            console.log(err1);
+        })
+
+
+}
+
+module.exports = {
+    AddNewUser,
+    UpdateUserData
+}
--- Backend/routers/LoginRouter.js
+++ Backend/routers/LoginRouter.js
@@ -2,6 +2,7 @@ const express = require('express');
 const Login_Router = express.Router();
 const Student = require("../Database/StudentsScheme");
 const multer = require('multer');
+const { AddNewUser, UpdateUserData } = require('../controlles/LoginController');
 
 
 //uploading file configurations 
@@ -19,44 +20,9 @@ const upload = multer({ storage: storage });
 
 
 ///APIs
-Login_Router.post('/log', upload.single('profileImg'), (req, res) => {
-    console.log("posting data  ", req.body);
-    console.log("posting file  ", req.file);
-
-    const _std = new Student({
-        username: req.body.name,
-        age: req.body.age,
-        email: req.body.email,
-        city: req.body.city,
-        profileImg: req.file.filename
-    });
-    _std.save().then((result) => {
-        res.send(result);
-    }).catch((err) => {
-        console.log(err);
-    });
-});
+Login_Router.post('/log', upload.single('profileImg'), AddNewUser);
 
 
-Login_Router.post('/update/:id', upload.single('profileImg'), (req, res) => {
-    console.log(req.file);
-    Student.updateOne({ _id: req.params.id }, {
-            $set: {
-                username: req.body.name,
-                city: req.body.city,
-                age: req.body.age,
-                email: req.body.email,
-                profileImg: req.file.filename
-            }
-        })
-        .then((res1) => {
-            console.log(res1);
-            res.send(res1)
-        })
-        .catch((err1) => {
-            console.log(err1);
-        })
+Login_Router.post('/update/:id', upload.single('profileImg'), UpdateUserData);
 
-
-});
 module.exports = Login_Router;
